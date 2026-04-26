72c97b44e1f61df211014335d372159ec900082f
huma mahmood <huma.mahmood02@gmail.com>
2026-04-26 09:42:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountid = 12345;
+let accountmail = "humibutt11@gmail.com";
+var accountpassword = "password123";
+accpuntcity = "faisalabad";
+let accountstate = null;
+
+//accountid = 2;
+accountmail = "humamahmood@gmail.com";
+
+console.log(accountid, accountmail, accountpassword, accpuntcity);
+console.table([accountid, accountmail, accountpassword, accpuntcity, accountstate]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all code as modern version of JavaScript
+
+//alert("hello") we are using node.js
+let name = "John"; //string
+
+console.log(typeof "name");
